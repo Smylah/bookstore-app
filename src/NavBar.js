9c5e25dacdc8d7efd958b7eacc5ace2ad673fefc
unknown <fav4rl.ci@gmail.com>
2022-01-21 15:50:53 +0100

    navbar dark

--- src/NavBar.js
+++ src/NavBar.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom"
 const NavBar = () => {
   return (
     <div>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
           {/* <button
             className="navbar-toggler"
